44efe04c9fad25be6b62b95d113f95771c7678b0
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-06-26 19:08:51 +0800

    fix margin

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -12,6 +12,21 @@ $(function(){
 
 });
 
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
 $(document).ready(function(){
 
 $(".bg").interactive_bg(); // function call
